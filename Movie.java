614a6da00e1add961748bcb70e1c2ded147078ab
Koutoh <775guga@gmail.com>
2024-11-28 15:09:24 -0300

    Commit 10

--- Movie.java
+++ Movie.java
@@ -1,53 +1,54 @@
-// Classe Movie
 public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     public static final int CHILDRENS = 2;
-
+ 
     private String _title;
     private int _priceCode;
-
+ 
     public Movie(String title, int priceCode) {
-        _title = title;
-        _priceCode = priceCode;
+       _title = title;
+       _priceCode = priceCode;
     }
-
-    // Método que define a cobrança dependendo do tipo de filme e dias alugados
-    public double getCharge(int daysRented) {
-        double result = 0;
-
-        switch (_priceCode) {
-            case REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
-
-    // Método que retorna os pontos de locação de acordo com a política
-    public int getFrequentRenterPoints(int daysRented) {
-        int result = 1;
-        if (_priceCode == Movie.NEW_RELEASE && daysRented > 1)
-            result++;  // Bonus de pontos para filmes novos alugados por mais de 1 dia
-        return result;
-    }
-
-    // Outros métodos
+ 
     public String getTitle() {
-        return _title;
+       return _title;
     }
-
+ 
     public int getPriceCode() {
-        return _priceCode;
+       return _priceCode;
     }
-}
+ 
+    public void setPriceCode(int arg) {
+       _priceCode = arg;
+    }
+ 
+    public double getCharge(int daysRented) {
+       double result = 0;
+       switch (getPriceCode()) {
+          case REGULAR:
+             result += 2;
+             if (daysRented > 2)
+                result += (daysRented - 2) * 1.5;
+             break;
+          case NEW_RELEASE:
+             result += daysRented * 3;
+             break;
+          case CHILDRENS:
+             result += 1.5;
+             if (daysRented > 3)
+                result += (daysRented - 3) * 1.5;
+             break;
+       }
+       return result;
+    }
+ 
+    public int getFrequentRenterPoints(int daysRented) {
+       int result = 1;
+       // Add bonus for a two-day new release rental
+       if ((getPriceCode() == NEW_RELEASE) && daysRented > 1)
+          result++;
+       return result;
+    }
+ }
+ 
--- Rental.java
+++ Rental.java
@@ -1,28 +1,25 @@
-// Classe Rental
 public class Rental {
    private Movie _movie;
    private int _daysRented;
 
    public Rental(Movie movie, int daysRented) {
-       _movie = movie;
-       _daysRented = daysRented;
+      _movie = movie;
+      _daysRented = daysRented;
    }
 
-   // Método modificado: agora apenas delega a responsabilidade para a classe Movie
-   public double getCharge() {
-       return _movie.getCharge(_daysRented);  // Chama o método em Movie
-   }
-
-   public int getFrequentRenterPoints() {
-       return _movie.getFrequentRenterPoints(_daysRented); // Retorna pontos de locação
-   }
-
-   // Outros métodos
    public Movie getMovie() {
-       return _movie;
+      return _movie;
    }
 
    public int getDaysRented() {
-       return _daysRented;
+      return _daysRented;
+   }
+
+   public double getCharge() {
+      return _movie.getCharge(_daysRented);
+   }
+
+   public int getFrequentRenterPoints() {
+      return _movie.getFrequentRenterPoints(_daysRented);
    }
 }
